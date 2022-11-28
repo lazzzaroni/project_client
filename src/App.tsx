4c2049a750ed72c1fd0fd7a027d4c2e1a92bbf27
Mikalai Paliakou <mpaliakou@exadel.com>
2022-11-28 14:40:25 +0100

    feat: make simple signup form and connect to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import axios from "axios";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleCreateUser(e: React.FormEvent) {
+    e.preventDefault();
+    await axios
+      .post("http://localhost:5000/signup", {
+        name: name,
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    setName("");
+    setEmail("");
+    setPassword("");
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Create an account</h1>
+      <form onSubmit={handleCreateUser}>
+        <label htmlFor="user-name">Name</label>
+        <input
+          type="text"
+          name="user-name"
+          id="user-name"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setName(e.target.value);
+          }}
+          value={name}
+        />
+        <label htmlFor="user-email">Email</label>
+        <input
+          type="email"
+          name="user-email"
+          id="user-email"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setEmail(e.target.value);
+          }}
+          value={email}
+          required
+        />
+        <label htmlFor="user-password">Password</label>
+        <input
+          type="password"
+          name="user-password"
+          id="user-password"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setPassword(e.target.value);
+          }}
+          value={password}
+          required
+        />
+        <button>Create Account</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
