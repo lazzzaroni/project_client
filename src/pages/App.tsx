401660da334ecc20cff539875fa6c6c85abe9454
Mikalai Paliakou <mpaliakou@exadel.com>
2022-12-05 15:54:10 +0100

    feat: add react-router-dom

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import { Login } from "./Login";
 
 export const App = () => {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
