a07bc64bb2b5bb1ee80cc0a0d75fe857c6516a23
Mikalai Paliakou <mpaliakou@exadel.com>
2022-12-12 16:20:29 +0100

    feat: pass token to to landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,15 @@
 import { Header } from "@/components";
+import { TokenContext } from "@/context/tokenContext";
+import { useContext } from "react";
 
 export const Landing = () => {
+  const token = useContext(TokenContext);
+
   return (
     <>
       <Header />
       <h1>Home page</h1>
+      {token ? <p>Token has been saved</p> : null}
     </>
   );
 };
