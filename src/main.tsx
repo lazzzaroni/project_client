0cb1f348fb94d566daa8cf67df3a31797f1ba68f
Mikalai Paliakou <mpaliakou@exadel.com>
2022-12-16 14:12:49 +0100

    feat: add loadable

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import loadable from "@loadable/component";
 import { lazy, StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const App = lazy(() => import("./pages/App"));
+const App = loadable(() => import("./pages/App"));
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
