e4d2121026b6891d9a8a7e673ef74d2872167c07
Mikalai Paliakou <mpaliakou@exadel.com>
2022-12-16 14:59:30 +0100

    feat: added basic ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ErrorBoundary } from "@/components/ErrorBoundary";
 import loadable from "@loadable/component";
 import { lazy, StrictMode } from "react";
 import ReactDOM from "react-dom/client";
@@ -7,6 +8,8 @@ const App = loadable(() => import("./pages/App"));
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 );
