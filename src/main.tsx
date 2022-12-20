2a3b46496c4c139c0d460bc17a11be2555fa68e1
Mikalai Paliakou <mpaliakou@exadel.com>
2022-12-20 19:15:07 +0100

    refactor: update react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,28 @@
 import { ErrorBoundary } from "@/components/ErrorBoundary";
-import loadable from "@loadable/component";
-import { lazy, StrictMode } from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css";
+import Login from "./pages/Login";
+import User from "./pages/User";
 
-const App = loadable(() => import("./pages/App"));
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Login />}>
+      <Route path="/user" element={<User />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <ErrorBoundary>
-      <App />
+      <RouterProvider router={router} />
     </ErrorBoundary>
   </StrictMode>
 );
